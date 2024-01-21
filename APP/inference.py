d9691f5c7dbe37a4e5ed3ff38a4d12ad2eec06a8
Praveen <95226524+praveendecode@users.noreply.github.com>
2024-01-21 14:44:23 +0530

    Add files via upload

---
+++ APP/inference.py
@@ -0,0 +1,48 @@
+import openai
+
+import os 
+
+from dotenv import load_dotenv, find_dotenv
+
+
+class OPEN_AI:
+
+    def get_completion(self,prompt='HI',temperature=0,model='gpt-3.5-turbo'):
+    
+        _ = load_dotenv(find_dotenv()) 
+
+        api = os.getenv('OPENAI_API_KEY')
+
+        client = openai.OpenAI(api_key='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')
+
+        response = client.chat.completions.create(
+            model = model,
+            messages = [{'role':'user','content':prompt}],
+            temperature = temperature
+        )
+
+        return response.choices[0].message.content
+    
+    def chatbot_completion(self,messages=[{'role':'user','content':'Hi Chat'}],temperature=0,model='gpt-3.5-turbo'):
+    
+        _ = load_dotenv(find_dotenv()) 
+
+        api = os.getenv('OPENAI_API_KEY')
+
+        client = openai.OpenAI(api_key=api)
+
+        response = client.chat.completions.create(
+            model = model,
+            messages = messages,
+            temperature = temperature
+        )
+
+        return response.choices[0].message.content
+    
+
+
+
+
+        
+
+  
